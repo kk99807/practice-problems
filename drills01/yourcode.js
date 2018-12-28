2a9169534247bb5ca7696d9a92158db6353a47d2
Karen King <kk99807@gmail.com>
2018-12-28 15:40:12 -0800

    Fully implemented

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,37 @@
 
-function sumArray(){
-	
+function sumArray( numbers ){
+    return numbers.reduce((runningSum, currentValue) => runningSum + currentValue);
 }
 
-function fitWithinVal(){
-
+function fitWithinVal( numbers, threshold ){
+    // Create a total with starting value 
+    var total = 0;
+    // Create an output list
+    var output = [];
+    // Get each number from start to end
+    numbers.forEach((currentNumber) => {
+        // if the sum of the total & the current number <= threshold
+        if (total + currentNumber <= threshold) {
+            // add the current number to the total
+            total += currentNumber;
+            // add the current number to the output
+            output.push(currentNumber);
+        }
+    });
+    
+    // Return the output 
+    return output;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan( namesArray, n ){
+    const findPassingNames = (output, name) => name.length < n ? output.concat([name]) : output;
+    return namesArray.reduce(findPassingNames, initialValue=[]);
 }
 
-function makeLabel(){
+function makeLabel( person ){
+    var address = person['home address'];
 
+    return `${person.greeting} ${person.givenName} ${person.familyName}
+${address.streetNumber} ${address.streetName}
+${address.city}, ${address.state} ${address.zip}`;
 }
